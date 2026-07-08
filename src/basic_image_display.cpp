07f032f48b78dd0995893c865ec4353c744f5847
Paintmc <christiansaliba46@gmail.com>
2026-07-08 15:05:12 -0400

    Color Problem Fixed!
    Turns out that the color issue was due to an incorrect physical wiring issue. Changed one pin assignment to make wiring less confusing.

--- src/basic_image_display.cpp
+++ src/basic_image_display.cpp
@@ -5,20 +5,20 @@
 
 // Pin Mapping
 
-#define R1_PIN 25
-#define G1_PIN 26
-#define B1_PIN 27
-#define R2_PIN 14
-#define G2_PIN 12
-#define B2_PIN 13
-#define A_PIN 21
-#define B_PIN 19
-#define C_PIN 5
-#define D_PIN 17
-#define E_PIN 33 // Problem!!!
-#define LAT_PIN 4
-#define OE_PIN 15
-#define CLK_PIN 16
+#define R1_PIN 25 
+#define G1_PIN 26 
+#define B1_PIN 27 
+#define R2_PIN 14 
+#define G2_PIN 12 
+#define B2_PIN 32 
+#define A_PIN 21 
+#define B_PIN 19 
+#define C_PIN 5 
+#define D_PIN 17 
+#define E_PIN 33   
+#define LAT_PIN 4 
+#define OE_PIN 15 
+#define CLK_PIN 16 
 
 // Config Object
 HUB75_I2S_CFG mxconfig;
@@ -57,9 +57,10 @@ void setup() {
 
 }
 
+// 
 
 void loop() {
   
-    dma_display->fillScreen(0xFFFF);
+    dma_display->fillScreen(0x0000);
 
 }
