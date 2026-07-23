d48b64ce7c785524ae49b77342f114916f4e6d10
Paintmc <christiansaliba46@gmail.com>
2026-07-23 03:31:38 -0400

    Extremely Basic Draw Function
    I experimented with using littlefs to load the gifs, but this didn't work as expected with the animatedgif library. Will look into it further but will most likely switch how the whole program works.

--- src/basic_image_display.cpp
+++ src/basic_image_display.cpp
@@ -37,28 +37,52 @@ HUB75_I2S_CFG mxconfig(
 MatrixPanel_I2S_DMA *dma_display = nullptr;
 
 
+AnimatedGIF gif;
+
+
+
 // Creating the draw function which will take an array of arrays of color values as input
 // and light up the corresponding LED's of the matrix
 
-void draw_animation(uint16_t (*animation)[64][64], int frame_count) {
+void draw_image(uint16_t image[]) {
     // size of one frame is already known (64x64) <- must change if your panel is of different size
-    for (int f = 0; f < frame_count; f++) {
-      for (int y = 0; y < 64; y++) {
-        for (int x = 0; x < 64; x++) {
-
-          dma_display->drawPixel(x, y, animation[f][y][x]);
-        }
+    int x = 0;
+    int y = 0;
+    for (int t = 0; t < 4096 ; t++) {
+      dma_display->drawPixel(x, y, image[t]);
+      x++;
+      if (x == 64) {
+        x = 0;
+        y++;
       }
-      delay(33); // <-- delay frames if you wish (change 33 to a value that suits your use)
-    } 
+    }
+    delay(33); // <-- delay frames if you wish (change 33 to a value that suits your use)
 }
 
+
+void GIFDraw(GIFDRAW *draw) {
+
+    //Prevents a crash that I was experiencing
+    if (dma_display == nullptr) return;
+
+
+  uint8_t *s = draw->pPixels;
+  uint16_t *colors = draw->pPalette;
+  int y = draw->iY + draw->y; //iY is band or chunk of a frame (0,10,...) and y is line in that chunk (0,1,2,...)
+  
+  for (int x = 0; x < draw->iWidth; x++) {
+
+    dma_display->drawPixel(x, y, colors[s[x]]);
+
+  }
+
+}
 
 void setup() {
 
     Serial.begin(115200);
-    
 
+    //Setting up the matrix
     mxconfig.driver = HUB75_I2S_CFG::FM6124; //For my specific Panel
     mxconfig.clkphase = false;
     mxconfig.latch_blanking = 4;
@@ -71,12 +95,22 @@ void setup() {
     //SET BRIGHTNESS:
     dma_display->setBrightness(155);
 
+    //Setting up LittleFS on the esp32
+    LittleFS.begin(true);
+    gif.begin(LITTLE_ENDIAN_PIXELS);
+
 }
+  
+
+
 
 
 void loop() {
-  // if frame count == 1 dont keep looping so have an if that has no code in it
-  // or if you want gif to loop then put draw_animation in here
-  //draw_animation();
+    
+  if (gif.open("/ezgif.com-gif-maker.gif", GIFDraw)) { 
+      Serial.println("GIF opened. Playing frames...");
+        
+      while (gif.playFrame(true, NULL)) {
+      }
 }
-
+}
